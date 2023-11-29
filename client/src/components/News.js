7557c0b05969f03b56c7c343551eb6ab76de1d5f
tobias <tobias.omondi@student.moringaschool.com>
2023-11-29 11:17:04 +0300

    updated feature

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -5,6 +5,7 @@ import images12 from '../components/images/Worst Drought in 60 Years Brings Fami
 import images13 from '../components/images/KOMMAAR Photo.jpeg'
 import images14 from '../components/images/African Boy at Lake Victoria Entebbe Uganda.jpeg'
 import './News.css'
+import Footer from './Footer'
 
 
 const News = () => {
@@ -18,11 +19,12 @@ const News = () => {
                 <span><a href="news1-full-article.html" className="read_more_button">Read More</a></span>
         </div>
         <div className='news_img_1'>
-            <img src={images11} alt='crying baby'/>
+        
             <h4>SHOWING LOVE TO THE NEEDY</h4>
             <p>Amidst the adversities faced by many, the campaign strives to be a beacon of light, offering a helping hand to children in vulnerable situations and the needy in our communities. 
                 The focus is not only on addressing immediate needs but also on fostering a sense of hope for a brighter future.</p>
-                <span><a href="news1-full-article.html" className="read_more_button">Read More</a></span>
+            <img src={images11} alt='crying baby'/>
+            <span><a href="news1-full-article.html" className="read_more_button">Read More</a></span>
         </div>
         <div className='news_img_2'>
             <img src={images12} alt=' a dying child'/>
@@ -32,10 +34,10 @@ const News = () => {
                 <span><a href="news1-full-article.html" className="read_more_button">Read More</a></span>
         </div>
         <div className='news_img_3'>
-        <img src={images13} alt=' happy child'/>
             <h4>HAPPINES BRINGS JOY TO CHILDREN</h4>
             <p>Amidst the adversities faced by many, the campaign strives to be a beacon of light, offering a helping hand to children in vulnerable situations and the needy in our communities. 
                 The focus is not only on addressing immediate needs but also on fostering a sense of hope for a brighter future.</p>   
+                <img src={images13} alt=' happy child'/>
                 <span><a href="news1-full-article.html" className="read_more_button">Read More</a></span>
         </div> 
         <div className='news_img_4'>
@@ -44,6 +46,9 @@ const News = () => {
             <p>Amidst the adversities faced by many, the campaign strives to be a beacon of light, offering a helping hand to children in vulnerable situations and the needy in our communities. 
                 The focus is not only on addressing immediate needs but also on fostering a sense of hope for a brighter future.</p>
                 <span><a href="news1-full-article.html" className="read_more_button">Read More</a></span>
+        </div>
+        <div>
+            <Footer/>
         </div>
         </div>
   )
