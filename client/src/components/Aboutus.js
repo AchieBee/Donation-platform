7225a330810ff44fe0e18c66b79d5f6b42ae1986
AchieBee <belza.achieng@student.moringaschool.combelza.achieng@student.moringaschool.com>
2023-11-30 12:27:25 +0300

    feat: adds Aboutus

--- client/src/components/Aboutus.js
+++ client/src/components/Aboutus.js
@@ -1,15 +1,26 @@
 import React from "react";
-import Navbar from './Navbar';
-
+import "./Aboutus.css";
 
 function Aboutus() {
+  const imageUrl = 'https://images.pexels.com/photos/5257759/pexels-photo-5257759.jpeg?auto=compress&cs=tinysrgb&w=600'
 
+  return (
+    <div className="aboutus-container">
+      <h3>About Us</h3>
+      <div className="header">
+        <h1>
+          <img src={imageUrl} alt="Our Organisation" />
+          <span className="overlay-text">Our Organisation</span>
+        </h1>
+      </div>
 
-    return (
-        <div>
-            <Navbar />
+      <div className="p-container highlighted">
+        <h4>About Us</h4>
+        <p><em>At HopeCharity we are an organisation....</em></p>
+      </div>
 
-        </div>
-    );
+    </div>
+  );
 }
+
 export default Aboutus;
