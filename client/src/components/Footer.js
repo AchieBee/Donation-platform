3508e2bf75c2095631ed5990fa56b532212cb8e5
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-11-30 09:14:51 +0300

    update5

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import images2 from '../components/images/Raúl Márquez on Twitter(1).jpeg'
+import logo from '../Assets/logo.png';
 import './Footer.css'
 import images3 from '../components/images/Elon Musk.jpeg'
 import images4 from '../components/images/Cuisenaire Rods in the Classroom.jpeg'
@@ -11,39 +11,37 @@ import images8 from '../components/images/Whatsapp icons for free download Freep
 const Footer = () => {
         return (
            <div className='footer'>
-            <div className='footer_section'>
-                <h4>CHARITY Organisation</h4>
-            </div>
-            <div className='footer_img'>
-                <img src={images2} alt='a child picture'/>
-                <p>Charity is a selfless act of giving, 
-                   driven by compassion and a desire to improve the well-being of
-                   others. It involves providing support, resources, or assistance to those 
-                   in need, fostering a sense of community and contributing to positive social impact</p>
-            </div>
-            <div className='footer_links'>
-            <a href='About us'><p>About us</p>
-                    </a>
-                    <a href='Contact info'><p>Contact us</p>
-                    </a> 
+                <div className='ftext'>
+                    <p>Charity is a selfless act of giving, 
+                    driven by compassion and a desire to improve the well-being of
+                    others. It involves providing support, resources, or assistance to those 
+                    in need, fostering a sense of community and contributing to positive social impact</p>
+                <img src={logo} alt="logo" className="logo" />
+                </div>
+                <div className='contact'>
+                    <a href='Contact info'><p>Contact us</p></a> 
                     <img className = 'mail' src={images7} alt= 'mail'/>
                     <img className='whatsapp' src = {images8} alt = 'whatsapp'/>
+                    <img className='linkedin' src={images6} alt='whatsapp' />
                     <span>+254741041580</span><br/>
                     <span>+254700909000</span><br/>
                     <a href='News'><p>News</p> </a>
-            </div>
-            <div className='btn_donation'>
-                <button type='Dontion'>Dontion Now</button>
-            </div>
-            <div className='socialmedia'>
-                <img src={images3} alt='twitter'/>
-                <img src= {images4} alt = 'instagram'/>
-                <img src = {images5} alt = 'facebook'/>
-                 <img src= {images6} alt='linked'/>
-             </div>
-             <div>
-             <p>&copy; 2023 Donation platform Organisation</p>
-             </div>
+                </div>
+                <div className='about'>
+                    <a href='About us'><p>About us</p></a>
+                </div>
+                <div className='btn_donation'>
+                    <button type='Dontion'>Donate Now</button>
+                </div>
+                <div className='socialmedia'>
+                    <img src={images3} alt='twitter'/>
+                    <img src= {images4} alt = 'instagram'/>
+                    <img src = {images5} alt = 'facebook'/>
+                    <img src= {images6} alt='linked'/>
+                </div>
+                <div className='fin'>
+                <p>&copy;2023 Hope Charity Organisation</p>
+                </div>
            </div> 
         )
     
--- client/src/components/Vision.js
+++ client/src/components/Vision.js
@@ -1,37 +1,45 @@
 import React from 'react'
 import './Vision.css'
+
+import Navbar from './Navbar';
 import images from '../components/images/Death of an infant shows cruelty of starving in a country of food abundance.jpeg'
 import Footer from './Footer'
+import drop1 from '../Assets/vision.png';
+import drop2 from '../Assets/mission.png';
+
 
 
 const Vision = () => {
   return (
-    <div className='container'>
+    <>
+        <Navbar />
         <div className='container-1'>
-            <h2>OUR MISSION AND VISSION STATEMENT</h2>
+            <h2></h2>
+        <div className='navv'>
+            ss
         </div>
-        <div className='mission'>
-            <h1>Mission</h1>
-            <p>"In pursuit of our mission, we are committed to empowering positive change through acts of compassion and collaboration.<br/> 
-                We strive to create a tangible impact on the lives of those in need, fostering a community where transparency,<br/> 
-                accountability, and unwavering dedication are the cornerstones of our endeavors.<br/> 
-                Join us on this mission to build a brighter, more compassionate future, one where every individual has the opportunity <br/> 
-                to thrive and contribute to a world defined by empathy and collective support."</p>
+        <div className='div2'>
+            <div className='mission'>
+                <img src={drop1} alt="drop1" className="drop1" />
+                <h2>Mission</h2>
+                <p>"In pursuit of our mission, we are committed to empowering positive change through acts of compassion and collaboration.<br/> 
+                    We strive to create a tangible impact on the lives of those in need, fostering a community where transparency,<br/> 
+                    accountability, and unwavering dedication are the cornerstones of our endeavors."</p>
+            </div>
+            <div className='vission'>
+                <img src={drop2} alt="drop2" className="drop2" />
+                <h2>Vision</h2>
+                <p>"In our vision, we see a world transformed by compassion, where every act of kindness ripples through communities,<br/> 
+                    breaking barriers and creating a tapestry of shared humanity. It's a future where hope is abundant, opportunities <br/> 
+                    are accessible to all, and the power of collective empathy shapes a world that nurtures the well-being and potential of every individual."</p>
+            </div>
         </div>
-        <div className='vission'>
-            <h1>Vision</h1>
-            <p>"In our vision, we see a world transformed by compassion, where every act of kindness ripples through communities,<br/> 
-                breaking barriers and creating a tapestry of shared humanity. It's a future where hope is abundant, opportunities <br/> 
-                are accessible to all, and the power of collective empathy shapes a world that nurtures the well-being and potential of every individual."</p>
-        </div>
-        <hr></hr>
         <div className='img'>
-        <img src={images} alt="Image link" />
-        <div>
-            <Footer />
+                  <img src={images} alt="Image link" className='img' />
         </div>
         </div>
-    </div>
+        <Footer />
+    </>
   )
 }
 
