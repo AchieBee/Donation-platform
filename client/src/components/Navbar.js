e1a556683d11d4fdb1f69268f2db60205bbfc084
tobias <tobias.omondi@student.moringaschool.com>
2023-12-08 22:50:31 +0300

    updated homepage

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -17,24 +17,50 @@ function Navbar() {
         }
     };
 
+    function PagesDropdown(props) {
+        const [isOpen, setIsOpen] = useState(false);
+      
+        const handleDropdownOpen = () => setIsOpen(true);
+        const handleDropdownClose = () => setIsOpen(false);
+      
+        return (
+          <div className="dropdown">
+            <button onClick={handleDropdownOpen}>{props.title}</button>
+            {isOpen && (
+              <ul className="dropdown-content">
+                {props.children}
+              </ul>
+            )}
+          </div>
+        );
+      }
+      
+
     return (
-        <nav className="nav" >
-            <div className="logo">
-                <img src={logo} alt="logo" className="logo" />
-            </div>
-            <div id="nav-div">
-                <div className="nav-links">
-                    <Link to="/">Homepage</Link>
-                    <Link to="/aboutus">Aboutus</Link>
-                    <Link to="/vision">Vision</Link>
-                    <Link to="/news">News</Link>
-                    <Link to="/pages">Pages</Link>
-                </div>
-                <div className="btn">
-                    <button onClick={handleDonateClick}>Donate</button>
-                </div>
-            </div>
-        </nav>
+        <nav className="nav">
+  <div className="logo">
+    <img src={logo} alt="logo" className="logo" />
+  </div>
+  <div id="nav-div">
+    <div className="nav-links">
+      <Link to="/">Homepage</Link>
+      <Link to="/aboutus">Aboutus</Link>
+      <Link to="/vision">Vision</Link>
+      <Link to="/news">News</Link>
+      <PagesDropdown title="Pages">
+        <Link to="/vision">Vision</Link>
+        <Link to="/stories">Stories</Link>
+        <Link to="/community">Community</Link>
+        <Link to="/gallery">Gallery</Link>
+        <Link to="/page3">Page 5</Link>
+      </PagesDropdown>
+    </div>
+    <div className="btn">
+      <button onClick={handleDonateClick}>Donate</button>
+    </div>
+  </div>
+</nav>
+
     );
 }
 
